7d49f86b3b467d21cf7e09cc9b0c2ad8944c4d9a
AnneDuff <2430060@bart.ca>
2025-03-28 23:24:59 -0400

    Remise de l'inventaire.

--- TP1 Inventaire/TP1 Inventaire.c
+++ TP1 Inventaire/TP1 Inventaire.c
@@ -10,11 +10,9 @@
 static uint8_t* heap = NULL;
 static size_t heap_top = 0;
 
-void* allocate(size_t size);
-
 typedef struct Items {
 	char* name;
-	int valeur;
+	int value;
 }Items;
 
 typedef struct Node Node;
@@ -28,6 +26,13 @@ struct Node {
 typedef Node bagOfHolding;
 
 int itemsCount = 0;
+
+void* allocate(size_t size) {
+	size_t old_top = heap_top;
+	heap_top += size;
+	assert(heap_top <= HEAP_SIZE);
+	return &heap[old_top];
+}
 
 void insert(Node* head, Items* newData) {
 	Node* currNode = head;
@@ -53,149 +58,155 @@ void insert(Node* head, Items* newData) {
 	}
 }
 
-void delete(Node* head, Items* name) {
-	Node* temp = head;
-	int wasFound = 0;
-	while (temp != head->prev) {
-		if ((int)temp->data == name) { wasFound = 1; break; }
-		temp = temp->next;
+//will not work if node to remove is the tail
+void delete(Node* head, char* name) {
+	Node* currNode = head;
+	bool wasFound = false;
+	while (currNode != head->prev) {
+		if (currNode->data->name == name) {
+			wasFound = true; 
+			break; 
+		}
+		currNode = currNode->next;
 	}
 	if (!wasFound) {
-		return; //Error couldn’t find value
+		printf("The item hasn't been found :(\n");
+		return;
 	}
-	temp->prev->next = temp->next;
-	temp->next->prev = temp->prev;
-	printf("\nThe item %s has been removed from the bag of holding.\n", name->name);
+	if (currNode == head) {
+		Node* swap = currNode->data;
+		currNode->data = currNode->next->data;
+		currNode->next->data = swap;
+		currNode = currNode->next;
+	}
+	currNode->prev->next = currNode->next;
+	currNode->next->prev = currNode->prev;
+	itemsCount -= 1;
+	printf("\nThe item %s has been removed from the bag of holding.\n", currNode->data->name);
 }
 
-void displayInventory(bagOfHolding* head) {
+void displayInventory(Node* head) {
 	printf("\nInventory:\n-----\n");
-	Node* temp = head;
-
-
-	while (temp != head->prev) {
-		/*if (temp->data == NULL) {
+	Node* currNode = head;
+	while (currNode != head->prev) {
+		if (currNode->data == NULL) {
 			printf("This spot is empty or has been deleted.\n");
-		}*/
-		printf("Item: %s\nValue: %d\n", temp->data->name, temp->data->valeur);
-		temp = temp->next;
+		}
+		printf("Item: %s\nValue: %dGP\n", currNode->data->name, currNode->data->value);
+		currNode = currNode->next;
 		printf("-----\n");
 	}
 	printf("\nNombre d'items: %d\n", itemsCount);
 }
 
-void sort(Node *head) {
+//sorts items according to their values
+void sort(Node* head) {
 	if (head == NULL) {
 		return;
 	}
 	Node* currNode = head;
-	bool swapped = false;
-	for (int i = 0; i < itemsCount - 2; i++) {
-		if (currNode->data->valeur > currNode->next->data->valeur) {
-			Items* swap = currNode->next->data;
-			currNode->next->data = currNode->data;
-			currNode->data = swap;
-			swapped = true;
+	bool swapped = true;
+	while (swapped) {
+		swapped = false;
+		currNode = head;
+		while (currNode->next != NULL) {
+			if (currNode->data->value > currNode->next->data->value) {
+				Items* swap = currNode->data;
+				currNode->data = currNode->next->data;
+				currNode->next->data = swap;
+				swapped = true;
+			}
+			currNode = currNode->next;
 		}
-		if (i >= itemsCount - 1 && swapped == true) {
-			swapped = false;
-			i = -1;
-			currNode = head;
-			currNode->next = head->next;
-		}
-		currNode = currNode->next;
 	}
+	printf("The bag of holding has been sorted.\n");
 }
 
-Node* search(Node* head, int value) {
-	Node* temp = head->prev;
-	while ((int)temp->data != value) {
-		temp = temp->next;
-		if (temp == head->prev) {
-			temp = NULL;
+//finds items relative to their positions
+void findItemPos(Node* position) {
+	printf("Item found! It is: %s\n", position->data->name);
+}
+
+//finds items relative to their names
+void findItemName(Node* head, char* itemName) {
+	Node* currNode = head;
+	while (currNode != head->prev) {
+		if (currNode->data->name == itemName) {
+			printf("Item found! It's name is: %s\n", currNode->data->name);
 			break;
 		}
 	}
-	return temp;
 }
-
-void* allocate(size_t size) {
-	size_t old_top = heap_top;
-	heap_top += size;
-	assert(heap_top <= HEAP_SIZE);
-	return &heap[old_top];
-}
-
 
 void main(void) {
 	heap = (uint8_t*)malloc(HEAP_SIZE);
 	assert(heap != NULL);
-#define NUM_ITEMS 6
+#define NUM_ITEMS 10
 	srand(time(NULL));
 
 	Items fiole = {
 		.name = "fiole",
-		.valeur = 5
-	};
-	Items torch = {
-		.name = "torch",
-		.valeur = 10
+		.value = 5
 	};
 	Items map = {
 		.name = "map",
-		.valeur = 30
+		.value = 30
 	};
 	Items staff = {
 		.name = "staff",
-		.valeur = 100
+		.value = 100
 	};
 	Items supplies = {
 		.name = "supplies",
-		.valeur = 10
+		.value = 10
 	};
 	Items spellbook = {
 		.name = "spellbook",
-		.valeur = 150
+		.value = 150
 	};
 	Items ring = {
 		.name = "ring",
-		.valeur = 1000
+		.value = 1000
 	};
-	/*Items boots = {
+	Items boots = {
 		.name = "boots",
-		.valeur = 25
+		.value = 25
 	};
 	Items chainmail = {
 		.name = "chainmail",
-		.valeur = 200
+		.value = 200
 	};
 	Items sword = {
 		.name = "sword",
-		.valeur = 50
-	};
-	Items potion = {
-		.name = "potion",
-		.valeur = 10
+		.value = 50
 	};
 	Items shield = {
 		.name = "shield",
-		.valeur = 100
+		.value = 100
 	};
 	Items apple = {
 		.name = "apple",
-		.valeur = 1
-	};*/
-	Items possible_items[NUM_ITEMS] = {torch, map, staff, supplies, spellbook, ring};
+		.value = 1
+	};
+	Items possible_items[NUM_ITEMS] = {
+		map, staff, supplies, spellbook, ring,
+		boots, chainmail, sword, shield, apple}; 
+
 	bagOfHolding head = { 0 };
 	
 	insert(&head, &fiole);
 	
-	for (int i = 0; i < (rand() % 10) + 1; i++) {
+	for (int i = 0; i < (rand() % 5) + 5; i++) {
 		insert(&head, &possible_items[rand() % NUM_ITEMS]);
 	}
 
 	displayInventory(&head);
+	delete(&head, "fiole");
 	sort(&head);
-	//delete(&head, &staff);
 	displayInventory(&head);
+
+
+
+	//findItemPos(&head);
+	//findItemName(&head, "fiole");
 }
